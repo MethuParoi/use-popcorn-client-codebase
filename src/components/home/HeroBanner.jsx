34e8f15b5928a80f13e90145042cc427c51ab093
MethuParoi <paroimethu@gmail.com>
2024-12-04 22:46:34 +0600

    hero slider added

---
+++ src/components/home/HeroBanner.jsx
@@ -0,0 +1,13 @@
+function HeroBanner({ source }) {
+  return (
+    <div className="flex items-center justify-center">
+      <img
+        className="w-screen object-cover lg:h-[30rem] h-[20rem] select-none pointer-events-none"
+        src={source}
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default HeroBanner;
